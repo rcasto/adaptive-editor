e46e24b394962c2baa93a268aee91d6dba7e5968
Richie <rcasto@users.noreply.github.com>
2018-03-11 15:03:54 -0700

    add and use helmet

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var path = require('path');
 var express = require('express');
+var helmet = require('helmet');
 var compression = require('compression');
 
 var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
