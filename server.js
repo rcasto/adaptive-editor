a11ecc32cc147020c94ded84b3cf7c28606cbc4b
Richie <rcasto@users.noreply.github.com>
2018-03-11 15:00:08 -0700

    add and use compression

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var path = require('path');
 var express = require('express');
+var compression = require('compression');
 
 var port = process.env.PORT || 3000;
 var app = express();
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
